54428d4dcc2d4c7c8969e6b98fc9fbf796d2dfb8
miikka <datahanska@gmail.com>
2014-09-02 11:41:17 +0300

    Custombindings valuutta ja prosentti

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,22 @@
 (function() {
   'use strict';
 
+  ko.bindingHandlers.valuutta = {
+    update: function(elem, valueAccessor) {
+      var luku = valueAccessor() || 0;
+      var muotoiltu = luku.toFixed(2);
+      $(elem).html('<span>' + muotoiltu + '€</span>');
+    }
+  };
+
+  ko.bindingHandlers.prosentti = {
+    update: function(elem, valueAccessor) {
+      var luku = valueAccessor() * 100 || 0;
+      var muotoiltu = luku.toFixed(0);
+      $(elem).html('<span>' + muotoiltu + '%</span>');
+    }
+  };
+
   var Veto = function(pelimuoto, panos, kerroin, voitto, kohteet) {
     var self = this;
     self.pelimuoto = pelimuoto;
