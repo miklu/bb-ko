54d502d54cfdd5c54e8ab47eac78b29643a781cc
miikka <datahanska@gmail.com>
2014-09-03 01:05:26 +0300

    Parempi gulpfile

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,42 +1,69 @@
+
+
+/******************************************************
+# RIIPPUVUUDET
+******************************************************/
 var gulp = require('gulp'),
-		less = require('gulp-less'),
-		autoprefixer = require('gulp-autoprefixer'),
-		minifycss = require('gulp-minify-css'),
-		rename = require('gulp-rename'),
-		uglify = require('gulp-uglify'),
-		concat = require('gulp-concat'),
-		livereload = require('gulp-livereload'),
-		path = require('path');
+    less = require('gulp-less'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
+    concat = require('gulp-concat'),
+    plumber = require('gulp-plumber'),
+    minifyCss = require('gulp-minify-css'),
+    autoprefixer = require('gulp-autoprefixer'),
+    livereload = require('gulp-livereload'),
+    path = require('path');
 
-gulp.task('styles', function() {
-	gulp.src('./public/css/*.less')
-		.pipe(less({
-			paths: [ path.join(__dirname, 'less', 'includes') ]
-		}))
-		.pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-		.pipe(gulp.dest('./public/css'))
-		.pipe(rename({suffix: '.min'}))
-		.pipe(minifycss())
-		.pipe(gulp.dest('./public/css'));
+/******************************************************
+# SIJAINNIT
+******************************************************/
+var kohde = {
+  less: 'public/less/styles.less',
+  css: 'public/css',
+  js: 'public/js/**/*.js',
+  html: 'views/**/*.html'
+};
+
+
+/******************************************************
+# LESS
+******************************************************/
+gulp.task('less', function() {
+  gulp.src(kohde.less)
+    .pipe(plumber())
+    .pipe(less({
+      paths: [ path.join(__dirname, 'less', 'includes')]
+    }))
+    .pipe(autoprefixer(
+      'last 2 version',
+      '> 1%',
+      'ie 8',
+      'ie 9',
+      'ios 6',
+      'android 4'
+    ))
+    .pipe(minifyCss())
+    .pipe(gulp.dest(kohde.css))
+    .pipe(livereload());
 });
 
-gulp.task('scripts', function() {
-	gulp.src('./public/js/jquery-2.1.1.min.js', './public/js/knockout-3.1.0.js', './public/js/main.js')
-		.pipe(concat('all.js'))
-		.pipe(gulp.dest('./public/js'))
-		.pipe(rename({suffix: '.min'}))
-		.pipe(uglify())
-		.pipe(gulp.dest('./public/js'));
+/******************************************************
+# HTML
+******************************************************/
+gulp.task('html', function() {
+  gulp.src(kohde.html)
+  .pipe(livereload());
 });
 
+/******************************************************
+# WATCH
+******************************************************/
 gulp.task('watch', function() {
-	// gulp.watch('./public/css/*.less', ['styles']);
-	// var server = livereload();
-	// gulp.watch(['./public/**', './views/*.html']).on('change', function(file) {
-	// 	server.changed(file.path);
-	// });
-	livereload.listen();
-	gulp.watch('public/css/**', ['styles']).on('change', livereload.changed);
+
+  var server = livereload();
+
+  gulp.watch(kohde.html, ['html']);
+  gulp.watch(kohde.less, ['less']);
 });
 
-gulp.task('default', ['styles']);
+gulp.task('default', ['less', 'watch']);
