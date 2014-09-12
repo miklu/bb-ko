99f77808c2c47dbc95b2285f2c6740525086abe6
miikka <datahanska@gmail.com>
2014-09-12 13:14:26 +0300

    Javascript filet erikseen sekä kombinaatiot

--- public/js/all.js
+++ public/js/all.js
@@ -121,6 +121,39 @@ ko.bindingHandlers.prosentti = {
 (function() {
   'use strict';
 
+  // Kombinaatio
+function k_combinations(set, k) {
+  var i, j, combs, head, tailcombs;
+  
+  if (k > set.length || k <= 0) {
+    return [];
+  }
+  
+  if (k == set.length) {
+    return [set];
+  }
+  
+  if (k == 1) {
+    combs = [];
+    for (i = 0; i < set.length; i++) {
+      combs.push([set[i]]);
+    }
+    return combs;
+  }
+  
+  // Assert {1 < k < set.length}
+  
+  combs = [];
+  for (i = 0; i < set.length - k + 1; i++) {
+    head = set.slice(i, i+1);
+    tailcombs = k_combinations(set.slice(i + 1), k - 1);
+    for (j = 0; j < tailcombs.length; j++) {
+      combs.push(head.concat(tailcombs[j]));
+    }
+  }
+  return combs;
+}
+
   var Veto = function(pelimuoto, panos, kerroin, voitto, kohteet) {
     var self = this;
     self.pelimuoto = pelimuoto;
@@ -169,6 +202,9 @@ ko.bindingHandlers.prosentti = {
     self.tallennusLomake = {pelimuoto: ko.observable(), panos: ko.observable(), voitto: ko.observable(), kerroin: ko.observable(), kohteet: ko.observableArray()};
     self.tallennusLomake.isVisible = ko.observable(false);  // Onko näkyvä
     self.tallennusLomake.isNew = ko.observable(false);      // Luodaanko uusi veto vai muokataanko
+
+    // Systeemit
+    self.sys = ko.observable(3);
 
     // Oletuksena lomakkeessa yksi kohde
     self.tallennusLomake.kohteet.push({ottelu: 'eka ottelu'});
@@ -238,6 +274,18 @@ ko.bindingHandlers.prosentti = {
           }
         });
       }
+    };
+
+    self.tallennaSysteemi = function(n) {
+      var kohteet = [
+        {ottelu: 'Eka', osuma: true},
+        {ottelu: 'Toka', osuma: true},
+        {ottelu: 'Kolmas', osuma: true},
+        {ottelu: 'Neljäs', osuma: true},
+      ];
+
+      console.log(k_combinations(kohteet, n));
+
     };
 
     // Lisää uuden kohteen lomakkeeseen
--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,39 @@
 (function() {
   'use strict';
 
+  // Kombinaatio
+function k_combinations(set, k) {
+  var i, j, combs, head, tailcombs;
+  
+  if (k > set.length || k <= 0) {
+    return [];
+  }
+  
+  if (k == set.length) {
+    return [set];
+  }
+  
+  if (k == 1) {
+    combs = [];
+    for (i = 0; i < set.length; i++) {
+      combs.push([set[i]]);
+    }
+    return combs;
+  }
+  
+  // Assert {1 < k < set.length}
+  
+  combs = [];
+  for (i = 0; i < set.length - k + 1; i++) {
+    head = set.slice(i, i+1);
+    tailcombs = k_combinations(set.slice(i + 1), k - 1);
+    for (j = 0; j < tailcombs.length; j++) {
+      combs.push(head.concat(tailcombs[j]));
+    }
+  }
+  return combs;
+}
+
   var Veto = function(pelimuoto, panos, kerroin, voitto, kohteet) {
     var self = this;
     self.pelimuoto = pelimuoto;
@@ -49,6 +82,9 @@
     self.tallennusLomake = {pelimuoto: ko.observable(), panos: ko.observable(), voitto: ko.observable(), kerroin: ko.observable(), kohteet: ko.observableArray()};
     self.tallennusLomake.isVisible = ko.observable(false);  // Onko näkyvä
     self.tallennusLomake.isNew = ko.observable(false);      // Luodaanko uusi veto vai muokataanko
+
+    // Systeemit
+    self.sys = ko.observable(2);
 
     // Oletuksena lomakkeessa yksi kohde
     self.tallennusLomake.kohteet.push({ottelu: 'eka ottelu'});
@@ -118,6 +154,30 @@
           }
         });
       }
+    };
+
+    self.tallennaSysteemi = function() {
+      // console.log(self.sys());
+
+
+      var kohteet = [
+        {ottelu: 'Eka', kerroin: 2},
+        {ottelu: 'Toka', kerroin: 4},
+        {ottelu: 'Kolmas', kerroin: 6},
+        {ottelu: 'Neljäs', kerroin: 8}
+      ];
+
+
+      var vedot = k_combinations(kohteet, self.sys());
+      for(var i=0; i < vedot.length; i++) {
+        var kerroin = 1;
+        for(var j=0; j < vedot[i].length; j++) {
+          console.log(vedot[i][j].ottelu);
+          kerroin *= vedot[i][j].kerroin;
+        }
+        console.log(kerroin);
+      }
+
     };
 
     // Lisää uuden kohteen lomakkeeseen
